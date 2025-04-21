9f654d518ec54d1c988a39cc4063c6783dad0b4f
Paul Ramsey <22893884+paulramsey@users.noreply.github.com>
2025-04-21 13:53:17 -0500

    chore: Migrate StyleSearch AlloyDB AI demo to dedicated repo.

--- cymbal-shops-alloydb/demo_app/ui/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
-
-export const routes: Routes = [
-    {path: 'products', component: ProductsComponent},
-    {path: '', redirectTo: '/products', pathMatch: 'full'}
-];
